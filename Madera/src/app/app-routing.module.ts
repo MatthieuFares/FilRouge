24c8e96897fbfc11ac9395399919f35a550e126f
Fares Matthieu <fares.matthieu@gmail.com>
2019-09-04 22:15:53 +0200

    Page login Créée
    Problème sur les liens

--- Madera/src/app/app-routing.module.ts
+++ Madera/src/app/app-routing.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './services/Auth-Guard.service';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
+  {
+    path: 'dashboard',
+    loadChildren: './dashboard/dashboard.module#DashboardPageModule',
+    canActivate: [AuthGuard]
+  }
 ];
-
+ 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
