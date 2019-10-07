fe0c116568b4f65a8b0f1652ac11af22e3f2eee2
Fares Matthieu <fares.matthieu@gmail.com>
2019-10-07 11:04:29 +0200

    On peut deconnecter

--- Madera/src/app/menu/menu.page.ts
+++ Madera/src/app/menu/menu.page.ts
@@ -1,16 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MenuController, NavController } from '@ionic/angular';
+import { AuthenticationService } from '../services/Authentication.service';
 
 @Component({
   selector: 'page-menu',
   templateUrl: 'menu.page.html',
   styleUrls: ['menu.page.scss'],
 })
-export class MenuPage {
-  pageSupprimer: any;
+export class MenuPage implements OnInit {
 
-  constructor(public navCtrl: NavController) { 
+  constructor(
+    private authService: AuthenticationService
+    ) { }
+
+  ngOnInit() {
+  }
+
+  logoutUser(){
+    this.authService.logout();
   }
 
 }
-
