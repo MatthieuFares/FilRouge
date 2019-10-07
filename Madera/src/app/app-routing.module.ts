4289f56a1830755b47ec2a2585c44817e117904c
Sulyvan <susu76@hotmail.fr>
2019-10-07 10:53:43 +0200

    Auto stash before merge of "Continue" and "origin/Continue"

--- Madera/src/app/app-routing.module.ts
+++ Madera/src/app/app-routing.module.ts
@@ -5,8 +5,13 @@ import { AuthGuard } from './services/auth-Guard.service';
 const routes: Routes = [
   { path: '', redirectTo: 'menu', pathMatch: 'full' },
   { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
+<<<<<<< Updated upstream
   { path: 'menu', loadChildren: './menu/menu.module#MenuPageModule' },
   { path: 'dashboard',loadChildren: './dashboard/dashboard.module#DashboardPageModule',canActivate: [AuthGuard]},
+=======
+  { path: 'menu', loadChildren: './menu/menu.module#MenuPageModule', canActivate: [AuthGuard] },
+  { path: 'dashboard', loadChildren: './dashboard/dashboard.module#DashboardPageModule', canActivate: [AuthGuard] }
+>>>>>>> Stashed changes
 ];
  
 @NgModule({
--- Madera/src/app/menu/menu.page.ts
+++ Madera/src/app/menu/menu.page.ts
@@ -1,20 +1,24 @@
 import { Component } from '@angular/core';
 import { MenuController, NavController } from '@ionic/angular';
 
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
 @Component({
   selector: 'page-menu',
   templateUrl: 'menu.page.html',
   styleUrls: ['menu.page.scss'],
 })
+<<<<<<< Updated upstream
 export class  MenuPage {
+=======
+export class MenuPage {
+>>>>>>> Stashed changes
   pageSupprimer: any;
 
   constructor(public navCtrl: NavController) { 
   }
 
-  private showSupprimer() {
-    this.navCtrl.navigateForward('pageSupprimer');
-  }
 }
 
