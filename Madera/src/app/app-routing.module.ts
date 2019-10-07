5e19e68fb565513bcf33d06dd15172fb9ab46f93
Fares Matthieu <fares.matthieu@gmail.com>
2019-10-07 10:27:48 +0200

    lol

--- Madera/src/app/app-routing.module.ts
+++ Madera/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { AuthGuard } from './services/auth-Guard.service';
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
+  { path: 'menu', loadChildren: './menu/menu.module#MenuPageModule' },
   {
     path: 'dashboard',
     loadChildren: './dashboard/dashboard.module#DashboardPageModule',
