f99a198f93292dfc47e2011b5029df170dc4e831
Sulyvan <susu76@hotmail.fr>
2019-10-10 11:11:20 +0200

    Login fonctionnel

--- Madera/src/app/services/authentication.service.ts
+++ Madera/src/app/services/authentication.service.ts
@@ -3,12 +3,15 @@ import { Router } from '@angular/router';
 import { Storage } from '@ionic/storage';
 import { ToastController, Platform } from '@ionic/angular';
 import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { Component } from '@angular/core';
  
  
 @Injectable()
 export class AuthenticationService {
   status: string;
+  statusCo: Observable<any>;
+  error: string;
   
   authState = new BehaviorSubject(false);
  
@@ -17,12 +20,19 @@ export class AuthenticationService {
     private storage: Storage,
     private platform: Platform,
     public toastController: ToastController,
-    private httpClient: HttpClient
+    private httpClient: HttpClient,
+    private toastCtrl: ToastController
   ) {
-    this.platform.ready().then(() => {
-      this.ifLoggedIn();
-    });
-    this.status = '';
+      this.platform.ready().then(() => {
+        this.ifLoggedIn();
+      });
+      this.status = '';
+      this.data = '';
+      this.error = '';
+    /*this.statusCo = this.httpClient.get(`http://maderaproject.com/api/maderaapi/user/login.php?username=${this.user_id}&pwd=${this.user_mdp}`);
+    this.statusCo.subscribe(data => {
+      console.log('status : ', data)
+    });*/
   }
  
   ifLoggedIn() {
@@ -32,25 +42,41 @@ export class AuthenticationService {
       }
     });
   }
- 
- 
-  login(id:string,mdp:string) {
-    var dummy_response = {
+
+
+  login(id:string, mdp:string) {
+    var value = {
       user_id: id,
       user_mdp: mdp
     };
-    var cred = {
-      user_id: 'susu',
-      user_mdp: 'susu'
-    }
 
-    if((dummy_response.user_id == cred.user_id) && (dummy_response.user_mdp == cred.user_mdp)){
-      this.storage.set('USER_INFO', cred).then((response) => {
-        this.router.navigate(['menu']);
-        this.authState.next(true);
-      });
-    }
-  }
+    this.statusCo = this.httpClient.get(`http://maderaproject.com/api/maderaapi/user/login.php?username=${value.user_id}&pwd=${value.user_mdp}`);
+
+    this.statusCo.subscribe(
+      data => {
+        this.data = JSON.stringify(data);
+        console.log(data);
+
+        this.storage.set('USER_INFO').then((response) => {
+          this.router.navigate(['menu']);
+          this.authState.next(true);
+        });
+      },
+      err => {
+        this.error = `An error occurred, the data could not be retrieved: Status: ${err.status}, Message: ${err.statusText}`;
+        console.log(err);
+
+        let toast = this.toastCtrl.create({
+          message: 'Vos informations sont incorrectes !',
+          duration: 3000,
+          position: 'bottom',
+          color: 'danger'
+        }).then(toast => {
+          toast.present();
+        });
+      }
+    )
+  };
  
   logout() {
     this.storage.remove('USER_INFO').then(() => {
